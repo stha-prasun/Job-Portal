3776ef294c13f3fc71012696f1c5e0fdd7f8dd08
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-03-01 12:19:15 +0545

    Initialization of project

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+//cors
+const corsOption = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOption));
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`Example app listening on port ${port}`);
+});
