ba67e150be0216c1d9c330992788ff482730a499
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-03-04 13:20:01 +0545

    Frontend Initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-blue-400">Test</div>
+      <button className="btn">Default</button>
+    </>
+  );
+};
+
+export default App;
